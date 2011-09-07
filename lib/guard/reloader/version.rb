f83a1c8a96edecac315e0485e205441f7ca707e1
Fajar Firdaus <fajarmf@gmail.com>
2011-09-07 21:09:21 +0700

    add templates

--- lib/guard/reloader/version.rb
+++ lib/guard/reloader/version.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 module Guard
   module ReloaderVersion
-    VERSION = "0.1.0" unless defined? ReloaderVersion::VERSION
+    VERSION = "0.1.1" unless defined? ReloaderVersion::VERSION
   end
 end
